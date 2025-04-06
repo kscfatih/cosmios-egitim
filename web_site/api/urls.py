44291f0ed89bdeb758fab9513105066c9fbedca1
Fatih Kuşçu <fatihkusci252@gmail.com>
2025-04-06 13:57:43 +0300

    commit 3

--- web_site/api/urls.py
+++ web_site/api/urls.py
@@ -8,7 +8,14 @@ router.register(r'products',ProductModelViewSet, basename='product')
 
 
 urlpatterns = [
-
+    path('categories', CategoryApiView.as_view(), name='api-categories'),
+    path('categories/<int:id>', CategoryApiView.as_view(), name='api-categories-detail'),
+    path('products', ProductApiView.as_view(), name='products-api'),
+    path('productsf', products_api_view, name='productsf'),
+    path('category-generic', CategoryGenericViewListCreate.as_view(), name='categories'),
+    path('category-generic/<int:pk>', CategoryGenericView.as_view(), name='category-detail'),
+    path('delete-category/<int:pk>', CategoryDeleteView.as_view(), name='category-delete'),
+    path('update-category/<int:pk>', CategoryUpdateView.as_view(), name='update-category'),
     path('router/', include(router.urls))
     
 ]
